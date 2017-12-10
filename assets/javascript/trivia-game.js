8117b39df9667dbb444f44cb11fbc2bbbd6f62c3
zhagan <zhagan@gmail.com>
2017-12-10 13:50:05 -0700

    question tweek

--- assets/javascript/trivia-game.js
+++ assets/javascript/trivia-game.js
@@ -15,58 +15,52 @@ game = {
     //any quantity of questions and answer choices may be entered
     questions: [
         {
-            question: 'At which game has AI not yet learned to'
-                + ' best a human player?',
-            options: ['Go', 'Chess', 'StarCraft', 'Jeopardy', 'Atari games',
-                'Poker'],
-            answer: 2,
-            image: 'q0.jpg',
-            link: 'https://www.technologyreview.com/s/609242/humans-are-still'
-                + '-better-than-ai-at-starcraftfor-now/',
-        },
-        {
-            question: 'Google\'s PageRank algorithm determines the relevance of'
-                + ' a website by measuring how interconnected it is to other'
-                + ' sites on the web.<br><br>Which is the <em>most recent</em>'
-                + ' application of the PageRank algorithm?',
-            options: ['predicting ecosystem collapse', 'ranking best athletes',
-                'debugging', 'de-ranking fake news', 'predicting traffic flow'],
-            answer: 3,
-            image: 'q1.jpg',
-            link: 'https://www.bloomberg.com/news/articles/2017-04-25/google'
-            + '-rewrites-its-powerful-search-rankings-to-bury-fake-news',
-        },
-        {
-            question: 'If "reddit.com" is using Hypertext Transfer Protocol'
-                + 'Secure (HTTPS) on all of its pages, which of the following'
-                + ' can your ISP, third-parties, or network sniffers access?',
-            options: ['that I visited reddit', 'my posts', 'my password',
-                'that I visited r/dogs'],
+            question: 'Which mountain has the highest paved road in'
+                + ' North America?',
+            options: ['Mt Evans', 'Pikes Peak', 'Mt Shasta', 'Mt Ranier', 'Smoky Mountain',
+                'Mt Rushmore'],
             answer: 0,
+            image: 'q0.jpg',
+            // link: 'https://www.technologyreview.com/s/609242/humans-are-still'
+            //     + '-better-than-ai-at-starcraftfor-now/',
+        },
+        {
+            question: 'Which food does Denver claim was invented whithin the'
+                + ' municpality in 1935',
+            options: ['Shrimp Cocktail', 'Rocky Mountain Oysters',
+                'CheeseBurger', 'Green Chile', 'Corndogs'],
+            answer: 2,
+            image: 'q1.jpg',
+            // link: 'https://www.bloomberg.com/news/articles/2017-04-25/google'
+            // + '-rewrites-its-powerful-search-rankings-to-bury-fake-news',
+        },
+        {
+            question: 'Who owns 1/3 of all the land in Colorado',
+            options: ['The Denver Broncos', 'Catholic Church', 'US Government',
+                'Coors'],
+            answer: 2,
             image: 'q2.jpg',
-            link: 'https://medium.com/mozilla-internet-citizen/how-does-https'
-                + '-protect-you-and-how-doesnt-it-6c785884a130',
+            // link: 'https://medium.com/mozilla-internet-citizen/how-does-https'
+            //     + '-protect-you-and-how-doesnt-it-6c785884a130',
         },
         {
-            question: 'Cookies are text files stored on your hard drive to'
-                + ' store login info, to save shopping carts, or to create'
-                + ' targeted ads.<br><br>What company invented cookies,'
-                + ' Javascript, and SSL encryption?',
-            options: ['Google', 'Mozilla', 'Facebook', 'Netscape', 'Microsoft'],
-            answer: 3,
-            image: 'q3.jpg',
-            link: 'http://blog.kameleoon.com/en/types-web-cookies/',
-        },
-        {
-            question: 'Which of these <em>cannot</em> be recognized using'
-                + ' Google\'s Cloud Vision API, which uses algorithms to'
-                + ' determine the content of an uploaded image?',
-            options: ['faces and emotions',  'species of plants',
-                'text and language', 'nudity and violence',
-                'geographic landmarks'],
+            question: 'What mountain view inspired Katherine Lee Bates\'s'
+                + ' America The Beautiful?',
+            options: ['Mt Evans', 'Pikes Peak', 'Mt Shasta', 'Mt Ranier', 'Smoky Mountain',
+                    'Mt Rushmore'],
             answer: 1,
+            image: 'q3.jpg',
+            // link: 'http://blog.kameleoon.com/en/types-web-cookies/',
+        },
+        {
+            question: 'Which city has the largest parks system in'
+                + ' the nation?',
+            options: ['Colorado Springs',  'Fort Collins',
+                'Boulder', 'Parker',
+                'Denver'],
+            answer: 4,
             image: 'q4.jpg',
-            link: 'https://techcrunch.com/2016/02/18/google-opens-its-cloud-vision-api-to-all-developers/',
+            // link: 'https://techcrunch.com/2016/02/18/google-opens-its-cloud-vision-api-to-all-developers/',
         },
         {
             question: 'Which of these quantites is the largest according to'
@@ -76,55 +70,54 @@ game = {
                 'litres of water in Earth\'s oceans'],
             answer: 1,
             image: 'q5.jpg',
-            link: 'https://www.lifewire.com/how-big-is-the-web-4065573',
+            // link: 'https://www.lifewire.com/how-big-is-the-web-4065573',
         },
         {
-            question: 'The world\'s largest data center currently in operation'
-                + ' is located in which country?',
-            options: ['Russia', 'United States', 'India', 'China'],
-            answer: 3,
+            question: 'Where is the worlds largest flat top mountain?',
+            options: ['Durango', 'Mesa Verde', 'Grand Mesa', 'Taos'],
+            answer: 2,
             image: 'q6.jpg',
-            link: 'https://www.racksolutions.com/news/data-center-news/top-10'
-                + '-largest-data-centers-world/',
+            // link: 'https://www.racksolutions.com/news/data-center-news/top-10'
+            //     + '-largest-data-centers-world/',
         },
-        {
-            question: 'Which entity houses the largest volume of digitized data'
-                + ' in one unique database?',
-            options: ['AT&T', 'US Governement', 'Google', 'Facebook'],
-            answer: 0,
-            image: 'q7.jpg',
-            link: 'https://www.waterfordtechnologies.com/big-data-interesting'
-                + '-facts/',
-        },
-        {
-            question: 'Apache maintains an open source processor for very large'
-                + ' data sets called Hadoop.<br><br>From what did Hadoop get'
-                + ' its name and elephant logo?',
-            options: ['a circus animal', 'a tribal story', 'a toy', 'a book'],
-            answer: 2,
-            image: 'q8.jpg',
-            link: 'https://www.sas.com/en_us/insights/big-data/hadoop.html',
-        },
-        {
-            question: 'What percentage of S&P 500 companies\' market value'
-                + ' comes from intangible assets, including data and software?',
-            options: ['22%', '57%', '84%', '91%'],
-            answer: 2,
-            image: 'q9.jpg',
-            link: 'http://files.technologyreview.com/whitepapers/MIT_Oracle'
-                + '+Report-The_Rise_of_Data_Capital.pdf?_ga=2.18019193.'
-                + '1751378999.1511739668-1844887473.1511623668',
-        },
-        {
-            question: 'The Turing test was devised to test whether humans can'
-                + ' distinguish between an AI program and another human. Turing'
-                + ' bet that the test would be passed by the year 2000.<br><br>'
-                + 'In what year did an AI pass the Turing test?',
-            options: ['1998', '2000', '2014', '2017', 'not yet passed'],
-            answer: 2,
-            image: 'q10.jpg',
-            link: 'http://time.com/2847900/eugene-goostman-turing-test/',
-        },
+        // {
+        //     question: 'Which entity houses the largest volume of digitized data'
+        //         + ' in one unique database?',
+        //     options: ['AT&T', 'US Governement', 'Google', 'Facebook'],
+        //     answer: 0,
+        //     image: 'q7.jpg',
+        //     link: 'https://www.waterfordtechnologies.com/big-data-interesting'
+        //         + '-facts/',
+        // },
+        // {
+        //     question: 'Apache maintains an open source processor for very large'
+        //         + ' data sets called Hadoop.<br><br>From what did Hadoop get'
+        //         + ' its name and elephant logo?',
+        //     options: ['a circus animal', 'a tribal story', 'a toy', 'a book'],
+        //     answer: 2,
+        //     image: 'q8.jpg',
+        //     link: 'https://www.sas.com/en_us/insights/big-data/hadoop.html',
+        // },
+        // {
+        //     question: 'What percentage of S&P 500 companies\' market value'
+        //         + ' comes from intangible assets, including data and software?',
+        //     options: ['22%', '57%', '84%', '91%'],
+        //     answer: 2,
+        //     image: 'q9.jpg',
+        //     link: 'http://files.technologyreview.com/whitepapers/MIT_Oracle'
+        //         + '+Report-The_Rise_of_Data_Capital.pdf?_ga=2.18019193.'
+        //         + '1751378999.1511739668-1844887473.1511623668',
+        // },
+        // {
+        //     question: 'The Turing test was devised to test whether humans can'
+        //         + ' distinguish between an AI program and another human. Turing'
+        //         + ' bet that the test would be passed by the year 2000.<br><br>'
+        //         + 'In what year did an AI pass the Turing test?',
+        //     options: ['1998', '2000', '2014', '2017', 'not yet passed'],
+        //     answer: 2,
+        //     image: 'q10.jpg',
+        //     link: 'http://time.com/2847900/eugene-goostman-turing-test/',
+        // },
     ],
 
     restart: function() {
